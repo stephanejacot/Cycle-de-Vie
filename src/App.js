dba17154945ac9705a6affcfdecc584026ae0518
Stephane Jacot <stephanejacot@icloud.com>
2019-04-15 17:00:12 +0200

    maj1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Film from './FormFilm';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Film />
+      </div>
+    )
+  }
+}
+
+export default App;
